4fe6d169f628263fc0431c9c29e5669fc2c3ff9a
Dan <dan.mok999@yandex.ru>
2026-05-09 20:57:58 +0500

    refactor

--- src/Noushima.Island/Actions/ActionRegistry.cs
+++ src/Noushima.Island/Actions/ActionRegistry.cs
@@ -20,12 +20,12 @@ public sealed class ActionRegistry
         .ToArray();
     }
 
-    public void Execute(Bot bot, float[] intetions, BotActionContext context)
+    public void Execute(Bot bot, float[] intentions, BotActionContext context)
     {
         var offset = 0;
         foreach (var action in actions)
         {
-            var actionOutput = intetions.AsSpan(offset, action.Size);
+            var actionOutput = intentions.AsSpan(offset, action.Size);
             if (actionOutput[0] > 0f)
             {
                 action.Execute(bot, actionOutput, context);
